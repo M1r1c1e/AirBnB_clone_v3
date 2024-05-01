510056c9daf7ebbecb871879726188e14d3f6eb7
M1r1c1e <andersonmiracle111@.com>
2024-05-01 12:57:36 -0700

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,31 +1,28 @@
 #!/usr/bin/python3
-
+"""flask app"""
+from os import getenv
 from flask import Flask, jsonify
 from flask_cors import CORS
-from os import getenv
 from api.v1.views import app_views
 from models import storage
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
-@app.teardown_appcontext
-def teardown(exception):
-    """
-    fuction to close storage session
-    """
-    storage.close()
 
 @app.errorhandler(404)
-def not_found(error):
-
-    """handling for 404 errors that returns 
-    a JSON-formatted 404 status code response.
-    """
-    
+def not_found404(error):
+    """ 404 respose handler"""
     return jsonify({"error": "Not found"}), 404
+
+
+@app.teardown_appcontext
+def storage_close(exception):
+    """storage section close handler."""
+    storage.close()
 
 
 if __name__ == "__main__":
